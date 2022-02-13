caf0405339e5ec36eea81432df55fad44cfa6a94
Antônio <antonio.fernandes.de.santana.neto@gmail.com>
2022-02-13 09:33:31 -0300

    delete backend

--- app-02/settings/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize('crud', 'root', '988766544', {
-	dialect: 'mysql',
-	host: 'localhost',
-	port: 3306
-})
-
-module.exports = sequelize
